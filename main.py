de4843a53e9141927795a0a8e34125b8fd123f16
boomtop <avlipro12@gmail.com>
2018-12-05 19:42:41 +0300

    Другая библиотека (редактирование легче)

--- main.py
+++ main.py
@@ -4,3 +4,22 @@
 3 добавить бд для выбора
 
 '''
+'''
+from mailmerge import MailMerge
+
+template = r'/home/boomtop/PycharmProjects/pypdf/ma.docx'
+
+document = MailMerge(template)
+print(document.get_merge_fields())
+
+document.merge(
+    text1='Тестовое поле!'
+)
+
+document.write('test1.docx')'''
+from docxtpl import DocxTemplate
+
+doc = DocxTemplate("ma.docx")
+context = {'text2': "Тестовый тест" }
+doc.render(context)
+doc.save("generated_doc.docx")
